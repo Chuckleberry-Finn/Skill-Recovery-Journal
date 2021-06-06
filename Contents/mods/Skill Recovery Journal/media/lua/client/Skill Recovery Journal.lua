c521be3fac27f4fd07ade7cb216fcb9949b9ac85
ChuckTheJittai <ChuckTheSheep@users.noreply.github.com>
2021-06-06 17:27:24 -0400

    Improve reading behavior and timings

--- Contents/mods/Skill Recovery Journal/media/lua/client/Skill Recovery Journal.lua
+++ Contents/mods/Skill Recovery Journal/media/lua/client/Skill Recovery Journal.lua
@@ -26,30 +26,57 @@ function ISReadABook:update()
 	STORED_ISReadABook_update(self)
 
 	--local jobDeltaBy10 = math.floor(self:getJobDelta()*10)
+	---@type IsoGameCharacter | IsoPlayer
+	local player = self.character
 
 	---@type Literature
 	local journal = self.item
 	local journalModData = journal:getModData()
 	local JMD = journalModData["SRJ"]
-	local skillLevels = JMD["skillLevels"]
-
-	---@type IsoGameCharacter | IsoPlayer
-	local player = self.character
-
 	local gainedXp = false
 
-	for skill,level in pairs(skillLevels) do
-		local currentPerkLevel = player:getPerkLevel(Perks[skill])
-		if currentPerkLevel < level then
-			player:getXp():AddXP(Perks[skill], currentPerkLevel+1)
-			gainedXp = true
+	local delayedStop = false
+	local sayText
+	local sayTextChoices = {"IGUI_PlayerText_DontUnderstand", "IGUI_PlayerText_TooComplicated", "IGUI_PlayerText_DontGet"}
+
+	if self.character:HasTrait("Illiterate") then
+		delayedStop = true
+		sayText = sayTextChoices[ZombRand(#sayTextChoices)+1]
+	end
+
+	if (not JMD) then
+		delayedStop = true
+		sayText = "There's nothing written here."
+	end
+
+	if not delayedStop then
+		local skillLevels = JMD["skillLevels"]
+
+		for skill,level in pairs(skillLevels) do
+			local currentPerkLevel = player:getPerkLevel(Perks[skill])
+			if currentPerkLevel < level then
+				player:getXp():AddXP(Perks[skill], currentPerkLevel+1)
+				gainedXp = true
+			end
+		end
+
+		if not gainedXp then
+			delayedStop = true
+			sayTextChoices = {"IGUI_PlayerText_KnowSkill","IGUI_PlayerText_BookObsolete"}
+			sayText = sayTextChoices[ZombRand(#sayTextChoices)+1]
+		else
+			self:resetJobDelta()
 		end
 	end
 
-	if not gainedXp then
-		self.action:setLoopedAction(false)
-	else
-		self:resetJobDelta()
+	if delayedStop then
+		if self.pageTimer >= self.maxTime then
+			self.pageTimer = 0
+			if sayText then
+				player:Say(getText(sayText), 0.55, 0.55, 0.55, UIFont.NewSmall, 0, "radio")
+			end
+			self:forceStop()
+		end
 	end
 end
 
@@ -58,9 +85,10 @@ STORED_ISReadABook_new = ISReadABook.new
 function ISReadABook:new(player, item, time)
 	local o = STORED_ISReadABook_new(self, player, item, time)
 
-	if o and item:getType() == "SkillRecoveryJournal" and (not player:isTimedActionInstant()) then
+	if o and item:getType() == "SkillRecoveryJournal" then
 		o.loopedAction = true
 		o.useProgressBar = false
+		o.maxTime = o.maxTime*0.05
 	end
 
 	return o
@@ -70,6 +98,10 @@ end
 ---@param recipe InventoryItem | Literature
 ---@param player IsoGameCharacter | IsoPlayer
 function SRJ.writeJournal(recipe, result, player)
+
+	if not player then
+		return
+	end
 
 	---@type InventoryItem | Literature
 	local oldJournal
@@ -83,15 +115,11 @@ function SRJ.writeJournal(recipe, result, player)
 		end
 	end
 
-	if not player then
-		return
-	end
-
 	local skillsRecord = ""
 
 	local recoverableSkills = SRJ.calculateGainedSkills(player)
 	if recoverableSkills == nil then
-		player:Say("I don't have any experiences to record.", 0.75, 0.75, 0.75, UIFont.NewSmall, 0, "radio")
+		player:Say("I don't have any experiences to record.", 0.55, 0.55, 0.55, UIFont.NewSmall, 0, "radio")
 		print("INFO: SkillRecoveryJournal: No recoverable skills to be saved.")
 		return
 	end
@@ -102,19 +130,20 @@ function SRJ.writeJournal(recipe, result, player)
 	journalModData["SRJ"] = journalModData["SRJ"] or {}
 	local JMD = journalModData["SRJ"]
 
-	if oldJournal then
-		if JMD then
-			if JMD["ID"] then
-				print("old journal found: JMD: "..JMD["ID"]["steamID"].."=  "..JMD["ID"]["userName"])
-			else
-				print("old journal found: JMD: ERR: no ID stored.")
-			end
-		end
-	end
-
 	JMD["skillLevels"] = JMD["skillLevels"] or {}
 	local storedSkills = JMD["skillLevels"]
-	JMD["ID"] = {["steamID"]=player:getSteamID(),["userName"]=player:getUsername()}
+
+	JMD["ID"] = JMD["ID"] or {}
+	local journalID = JMD["ID"]
+
+	local pSteamID = player:getSteamID()
+	local pOnlineID = player:getOnlineID()
+	print("-- SRJ INFO:".." pSteamID: "..pSteamID.." pOnlineID: "..pOnlineID.." --")
+
+	if pSteamID ~= 0 then
+		journalID["steamID"] = pSteamID
+	end
+	journalID["onlineID"] = pOnlineID
 
 	for skill,level in pairs(recoverableSkills) do
 		if level > 0 then
@@ -131,14 +160,13 @@ function SRJ.writeJournal(recipe, result, player)
 		skillsRecord = skillsRecord..perkName.."("..level..")\n"
 	end
 
-	print("INFO: SkillRecoveryJournal: "..tostring(JMD["ID"]["steamID"]).." = "..tostring(JMD["ID"]["userName"]).." = "..player:getFullName())
-
 	JMD["author"] = player:getFullName()
 	JMD["ToolTip"] = "\nA record of "..JMD["author"].."'s life.\n"..skillsRecord
 	journal:setTooltip(JMD["ToolTip"])
 end
 
 
+--TODO: Calculate gained XP rather than levels - causing issue with professions not being able to store max level gains
 function SRJ.calculateGainedSkills(player)
 
 	local bonusLevels = {}
@@ -180,7 +208,7 @@ function SRJ.calculateGainedSkills(player)
 	end
 
 	if not storingSkills then
-		return nil, nil
+		return
 	end
 
 	return gainedLevels
