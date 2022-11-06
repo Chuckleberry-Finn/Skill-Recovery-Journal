8f12032ac4f291268124e1274f63a0459e52c858
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-11-06 10:45:27 -0500

    internal version +

--- Contents/mods/Skill Recovery Journal/media/lua/client/Skill Recovery Journal Main.lua
+++ Contents/mods/Skill Recovery Journal/media/lua/client/Skill Recovery Journal Main.lua
@@ -1,4 +1,4 @@
-Events.OnGameBoot.Add(function() print("Skill Recovery Journal: ver:0.8-NOV-6") end)
+Events.OnGameBoot.Add(function() print("Skill Recovery Journal: ver:0.8-NOV-6-HOTFIX2") end)
 
 SRJ = {}
 
