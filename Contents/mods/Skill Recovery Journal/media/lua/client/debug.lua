c41813072e8373c8169d75d85bb8a8f7950f0328
Chuckleberry Finn <6071161+ChuckTheSheep@users.noreply.github.com>
2022-12-29 22:38:57 -0500

    deaden debug file

--- Contents/mods/Skill Recovery Journal/media/lua/client/debug.lua
+++ Contents/mods/Skill Recovery Journal/media/lua/client/debug.lua
@@ -1,11 +1,1 @@
-if getDebug() then
-	Events.OnCustomUIKey.Add(function(key)
-		---@type IsoPlayer | IsoGameCharacter | IsoGameCharacter | IsoLivingCharacter | IsoMovingObject player
-		local player = getSpecificPlayer(0)
-
-		if key == Keyboard.KEY_1 then
-			SRJ.calculateGainedSkills(player)
-		end
-
-	end)
-end
+---DEAD FILE
