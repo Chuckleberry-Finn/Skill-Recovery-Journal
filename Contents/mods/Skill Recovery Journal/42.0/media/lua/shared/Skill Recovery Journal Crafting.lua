dba877714009c90677e60282e2d0426568f95699
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-12-23 22:14:37 -0500

    crafting B42

--- Contents/mods/Skill Recovery Journal/42.0/media/lua/shared/Skill Recovery Journal Crafting.lua
+++ Contents/mods/Skill Recovery Journal/42.0/media/lua/shared/Skill Recovery Journal Crafting.lua
@@ -1,31 +1,24 @@
 local function SkillRecoveryJournalRecipe()
 
-    local sandboxOption = SandboxVars.SkillRecoveryJournal.CraftRecipe or "item 1 [Base.Notebook;Base.Journal] flags[Prop2] mode:destroy| item 1 tags[Glue] flags[Prop1]| item 3 [Base.LeatherStrips;Base.LeatherStripsDirty] mode:destroy| item 1 [Base.Thread;Base.Yarn]"
-    if not sandboxOption or sandboxOption == "" then return end
+    local sandboxOption = SandboxVars.SkillRecoveryJournal.CraftRecipe
+    local newScript
 
-    local script = {
-        header = "timedAction = Making, Time = 150, Tags = InHandCraft, category = Skill Journal, inputs { ",
-        footer = " } outputs { item 1 Base.SkillRecoveryBoundJournal, }",
-
-        --header = "{ inputs {",
-        --footer = "} }",
-    }
-
-    local modified_option = string.gsub(sandboxOption, "|", ",")
-    local newScript = script.header .. modified_option .. script.footer
+    if not sandboxOption or sandboxOption == "" then
+        newScript = "{ needTobeLearn = true, }"
+    else
+        local scriptBase = { header = "{ inputs { ", footer = " } }", }
+        local modified_option = string.gsub(sandboxOption, "|", ",")
+        newScript = scriptBase.header .. modified_option .. scriptBase.footer
+    end
 
     print("SCRIPT: ", newScript)
 
-    local scriptManager = getScriptManager()
-    local journalRecipe = CraftRecipe.new()--scriptManager:getCraftRecipe("BindSkillRecoveryJournal")
-    journalRecipe:setModule(scriptManager:getModule("Base"))
-    journalRecipe:InitLoadPP("BindSkillRecoveryJournal")
-    journalRecipe:Load("Base.BindSkillRecoveryJournal", newScript)
-
-    --local scriptManager = getScriptManager()
-    --scriptManager:ParseScript(newScript)
+    if newScript then
+        local scriptManager = getScriptManager()
+        local journalRecipe = scriptManager:getCraftRecipe("BindSkillRecoveryJournal")
+        journalRecipe:Load("BindSkillRecoveryJournal", newScript)
+    end
 end
 
-
-Events.OnLoad.Add(SkillRecoveryJournalRecipe)
+Events.OnInitWorld.Add(SkillRecoveryJournalRecipe)
 if isServer() then Events.OnGameBoot.Add(SkillRecoveryJournalRecipe) end
