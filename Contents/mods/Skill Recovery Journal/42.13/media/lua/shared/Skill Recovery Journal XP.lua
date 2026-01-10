c1bc4324bbfe0a02c43938a20045b604a1c6ccb5
MassCraxx <masscraxx@users.noreply.github.com>
2026-01-10 06:08:17 +0100

    remove verbose log

--- Contents/mods/Skill Recovery Journal/42.13/media/lua/shared/Skill Recovery Journal XP.lua
+++ Contents/mods/Skill Recovery Journal/42.13/media/lua/shared/Skill Recovery Journal XP.lua
@@ -364,7 +364,7 @@ end
 function SRJ_XPHandler.getPerkLevelAfterJournalRead(SRJ, player, perkID, multi, journalXP)
     local readXP = SRJ.modDataHandler.getReadXP(player)
 	local playerXP = player:getXp():getXP(Perks[perkID]) / multi
-	print("Player ", playerXP, " JournalXP ", journalXP, " Read ", readXP[perkID], " Multi ", multi)
+	--print("Player ", playerXP, " JournalXP ", journalXP, " Read ", readXP[perkID], " Multi ", multi)
 	local playerXPAfterFullRead = playerXP + math.max(0, journalXP - (readXP[perkID] or 0))
 	local level = SRJ.xpHandler.getPerkLevelFromXP(perkID, playerXPAfterFullRead * multi + 1)
 
