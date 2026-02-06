230c9feb2bdc50a1c5fdfdfe18c72fdbc8c419b5
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2026-02-06 18:29:20 -0500

    refactor SkillRecoveryJournalRecipe
    added a check for trailing comma (seems to have slipped in as a default)
    
    made the if-chain easier to read

--- Contents/mods/Skill Recovery Journal/42.13/media/lua/shared/Skill Recovery Journal Crafting.lua
+++ Contents/mods/Skill Recovery Journal/42.13/media/lua/shared/Skill Recovery Journal Crafting.lua
@@ -3,22 +3,27 @@ local function SkillRecoveryJournalRecipe()
     local defaultRecipe = "item 1 [Base.Notebook;Base.Journal;Base.Diary1;Base.Diary2;Base.Notepad] flags[Prop2] mode:destroy, item 1 tags[Glue] flags[Prop1], item 3 [Base.LeatherStrips;Base.LeatherStripsDirty] mode:destroy, item 1 [Base.Thread;Base.Yarn;Base.Twine],"
     local sandboxOption = SandboxVars.SkillRecoveryJournal.CraftRecipe
     local needToLearn = SandboxVars.SkillRecoveryJournal.CraftRecipeNeedLearn
-    local newScript
 
-    if not sandboxOption or sandboxOption == "" then
-        newScript = "{ NeedToBeLearn = ".. tostring(needToLearn == true) ..", inputs { " .. defaultRecipe .. " } }"
-    else
-        if getDebug() then print("Add SRF custom crafting recipe with inputs " .. sandboxOption) end
-        local modified_option = string.gsub(sandboxOption, "|", ",")
-        if modified_option:sub(-1) ~= "," then modified_option = modified_option .. "," end
+    --- Maybe a way to validate the recipe would be possible?
+    --correct old sandbox options
+    local modified_option = sandboxOption and string.gsub(sandboxOption, "|", ",")
+    --add missing comma that might be default for some older saves
+    if modified_option and modified_option:sub(-1) ~= "," then modified_option = modified_option .. "," end
 
-        newScript = "{ NeedToBeLearn = ".. tostring(needToLearn == true) ..", inputs { " .. modified_option .. " } }"
-    end
+    local inputs = (not sandboxOption or sandboxOption == "") and defaultRecipe or modified_option
+
+    local newScript = "{ NeedToBeLearn = ".. tostring(needToLearn == true) ..", inputs { " .. inputs .. " } }"
+
+    if getDebug() then print("[SRJ] Final Recipe Script: " .. newScript) end
 
     if newScript then
         local scriptManager = getScriptManager()
         local journalRecipe = scriptManager:getCraftRecipe("BindSkillRecoveryJournal")
-        journalRecipe:Load("BindSkillRecoveryJournal", newScript)
+        if journalRecipe then
+            journalRecipe:Load("BindSkillRecoveryJournal", newScript)
+        else
+            print("[SRJ] ERROR: Could not find CraftRecipe 'BindSkillRecoveryJournal'")
+        end
     end
 end
 
